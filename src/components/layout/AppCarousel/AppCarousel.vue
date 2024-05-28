9ad85bab099560326f017edbcf538389bc4b62c8
Ted not teddy <tedxweb@outlook.com>
2024-05-28 18:54:41 +0800

    fix: navbar upgrade to carousel

---
+++ src/components/layout/AppCarousel/AppCarousel.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from 'oooo-components/ui/carousel'
+import Autoplay from 'embla-carousel-autoplay'
+import AppCarouselItem from './AppCarouselItem.vue'
+import GOOOO_MOBILE_IMAGE from '@/assets/images/testnet/banner.mobile.png'
+import GOOOO_PC_IMAGE from '@/assets/images/testnet/banner.pc.png'
+
+const activies = [
+  {
+    name: 'Goooo Points',
+    imageMobile: GOOOO_MOBILE_IMAGE,
+    imagePC: GOOOO_PC_IMAGE,
+    url: 'https://app.galxe.com/quest/oooo/GCgn9tzHvd'
+  }
+]
+</script>
+
+<template>
+  <Carousel
+    class="app-navbar mx-[24px] md:mx-[48px] xl:mx-auto xl:max-w-[832px] xl:w-full"
+    v-if="activies.length > 0"
+    :plugins="[Autoplay()]"
+    :opts="{
+      loop: true
+    }"
+  >
+    <CarouselContent>
+      <CarouselItem
+        v-for="activity of activies"
+        :key="activity.name"
+      >
+        <AppCarouselItem
+          :link="activity.url"
+          :image-mobile="activity.imageMobile"
+          :image-p-c="activity.imagePC"
+        />
+      </CarouselItem>
+    </CarouselContent>
+    <div class="hidden md:block">
+      <CarouselPrevious />
+      <CarouselNext />
+    </div>
+  </Carousel>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/layout/AppCarousel/AppCarouselItem.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps<{
+  imageMobile: string
+  imagePC: string
+  link?: string
+}>()
+</script>
+
+<template>
+  <a
+    class="app-carousel__item cursor-pointer"
+    :href="props.link"
+    target="_blank"
+  />
+</template>
+
+<style lang="scss" scoped>
+.app-carousel__item {
+  display: block;
+  height: 0;
+  width: 100%;
+  background: no-repeat center/contain;
+
+  padding-bottom: 22.63%;
+  background-image: v-bind("`url('${props.imageMobile}')`");
+
+  @media (min-width: 768px) {
+    padding-bottom: 15.625%;
+    background-image: v-bind("`url('${props.imagePC}')`");
+  }
+}
+</style>
