7537cc6b41f225606b1efe45bd62175f22cbbb38
Ted not teddy <tedxweb@outlook.com>
2024-07-05 18:23:55 +0800

    fix: offline merlin activity

--- src/pages/goooo-points/components/EarnPoints/index.vue
+++ src/pages/goooo-points/components/EarnPoints/index.vue
@@ -6,7 +6,6 @@ import Airdrop from './Airdrop/index.vue'
 import TaskCard from './TaskCard.vue'
 
 import B2BridgeTask from './Tasks/B2BridgeTask.vue'
-import BinanceMerlinBridgeTask from './Tasks/BinanceMerlinBridgeTask.vue'
 import AlienxBridgeTask from './Tasks/AlienxBridgeTask.vue'
 
 // import TwitterTask from './Tasks/TwitterTask.vue'
@@ -42,7 +41,6 @@ import BRIDGE_TO_EARN_IMAGE from '@/assets/images/goooo-points/bridge.png'
     >
       <AlienxBridgeTask />
       <B2BridgeTask />
-      <BinanceMerlinBridgeTask />
     </TaskCard>
   </div>
 </template>
