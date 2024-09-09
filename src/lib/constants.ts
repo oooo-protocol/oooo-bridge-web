35c1c4e594f5988d320eb9514216fb7d56f785f0
Ted not teddy <tedxweb@outlook.com>
2024-09-09 11:50:39 +0800

    feat: add soneium and story chain support

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -60,7 +60,9 @@ import {
   ZORA_TESTNET,
   ZORA_LIVENET,
   OPBNB_TESTNET,
-  OPBNB_LIVENET
+  OPBNB_LIVENET,
+  SONEIUM_TESTNET,
+  STORY_TESTNET
 } from 'oooo-components/lib/chain-config'
 import { type ChainConfig } from 'oooo-components/oooo-wallet'
 
@@ -217,6 +219,14 @@ export const CHAIN_LIST = [
     value: CHAIN.OPBNB,
     [NETWORK.TESTNET]: OPBNB_TESTNET,
     [NETWORK.LIVENET]: OPBNB_LIVENET
+  }, {
+    image: 'https://oooo.money/static/images/soneium.png',
+    value: CHAIN.SONEIUM,
+    [NETWORK.TESTNET]: SONEIUM_TESTNET
+  }, {
+    image: 'https://oooo.money/static/images/story.png',
+    value: CHAIN.STORY,
+    [NETWORK.TESTNET]: STORY_TESTNET
   }
 ]
 
