c8b7ad86abbd14a2fed55ced4772fbf1959f92c8
Ted not teddy <tedxweb@outlook.com>
2024-09-27 23:12:10 +0800

    fix: offline lumoz activity, and add bob activity

--- src/pages/goooo-points/components/EarnPoints/Tasks/MacaronBridgeTask.vue
+++ src/pages/goooo-points/components/EarnPoints/Tasks/MacaronBridgeTask.vue
@@ -9,7 +9,7 @@ import TaskItem from '../TaskItem.vue'
     title="COMPLETE MACARON EVENT TO WIN Goooo AND o-VOUCHER"
     description="REWARDS: 2X MACARON DEFI POINTS, 8 Goooo, $20 o-VOUCHER PACK"
     :image="MACARON_IMAGE"
-    hint="+8 Goooo & +80 Lumoz Points"
+    hint="+8 Goooo"
   >
     <Button
       class="w-full md:w-[174px]"
